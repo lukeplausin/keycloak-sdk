11ef39f5b85f222dac23dbf6e597ef8aec69582d
Luke Plausin <luke.plausin@gmail.com>
2018-01-30 17:51:10 +0100

    Added a setup.py file to create the pip installable module

---
+++ setup.py
@@ -0,0 +1,85 @@
+"""
+
+An SDK for Keycloak written in Python.
+
+See:
+http://www.keycloak.org/docs-api/3.4/rest-api/index.html#_overview
+
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+# from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+# with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+#     long_description = f.read()
+long_description = "An SDK for Keycloak written in Python."
+
+# Arguments marked as "Required" below must be included for upload to PyPI.
+# Fields marked as "Optional" may be commented out.
+
+setup(
+    name='keycloak-sdk',
+    version='0.9.0',
+    description="An SDK for Keycloak written in Python.",
+    long_description=long_description,
+
+    # This should be a valid link to your project's main homepage.
+    #
+    # This field corresponds to the "Home-Page" metadata field:
+    # https://packaging.python.org/specifications/core-metadata/#home-page-optional
+    # url='https://github.com/pypa/sampleproject',  # Optional
+    author='Luke Plausin',
+    # author_email='pypa-dev@googlegroups.com',  # Optional
+
+    # Classifiers help users find your project by categorizing it.
+    #
+    # For a list of valid classifiers, see
+    # https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[  # Optional
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Security',
+        'Topic :: System :: Systems Administration',
+
+        # Pick your license as you wish
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+
+    keywords='keycloak sdk api signon security admin administration',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    install_requires=['requests'],
+
+    # List additional groups of dependencies here (e.g. development
+    # dependencies). Users will be able to install these using the "extras"
+    # syntax, for example:
+    #
+    #   $ pip install sampleproject[dev]
+    #
+    # Similar to `install_requires` above, these must be valid existing
+    # projects.
+    extras_require={  # Optional
+        'dev': ['beautifulsoup4', 'jinja2'],
+        'test': ['coverage'],
+    },
+)
